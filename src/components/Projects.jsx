80dadfed8872ade27d5d5a8ba36902b5477e344f
arunsaab <arunsinghsaab98@gmail.com>
2024-12-07 15:44:50 +0530

    making visibility of project name on smaller screen more better

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,20 +23,24 @@ const Projects = () => {
             key={project.id}
             className="group relative overflow-hidden rounded-3xl"
           >
+            {/* Image */}
             <motion.img
               whileHover={{ scale: 1.1 }}
               src={project.image}
               alt={project.name}
               className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
             />
+            {/* Content (Title & Description) */}
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.5 }}
-              className="absolute inset-0 flex flex-col items-center text-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity  duration-500 group-hover:opacity-100"
+              className="absolute inset-0 flex flex-col items-center text-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100 px-4 py-8"
             >
               <h3 className="mb-2 text-2xl font-bold">{project.name}</h3>
-              <p className="mb-12 p-4">{project.description}</p>
+              <div className="max-h-[200px] overflow-y-auto p-4">
+                <p className="mb-12">{project.description}</p>
+              </div>
             </motion.div>
           </motion.div>
         ))}
